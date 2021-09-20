ac18422b33654efccf576ca091983de33d3c8db5
David John Gagne <davidjohngagne@gmail.com>
2021-09-20 09:32:55 -0600

    Added initial base files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="echo-opt",
+      version="0.1",
+      description="Earth Computer Hyperparameter Optimization",
+      author="John Schreck, David John Gagne, Charlie Becker, Gabrielle Gantos, Keely Lawrence",
+      license="MIT",
+      url="https://github.com/NCAR/echo-opt",
+      packages=["echo"],
+      )
