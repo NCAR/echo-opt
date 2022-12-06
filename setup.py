0da095ea4aa8454ba8a3b8e88b5edb9515a6f07a
David John Gagne <davidjohngagne@gmail.com>
2022-12-06 14:24:26 -0700

    Added setup.cfg file and updated requirements to reflect libraries being imported by echo-opt.

--- setup.py
+++ setup.py
@@ -1,13 +1,4 @@
 from setuptools import setup
 
-setup(name="echo-opt",
-      version="0.1",
-      description="Earth Computer Hyperparameter Optimization",
-      author="John Schreck, David John Gagne, Charlie Becker, Gabrielle Gantos, Keely Lawrence",
-      license="MIT",
-      url="https://github.com/NCAR/echo-opt",
-      packages=["echo", "echo/src"],
-      entry_points = {
-        'console_scripts': ['echo-opt=echo.optimize:main', 'echo-run=echo.run:main', 'echo-report=echo.report:main'],
-      }
-      )
+if __name__ == "__main__":
+    setup()
