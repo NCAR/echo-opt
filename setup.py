f80830a26cd2a3e24348731a6c2e28cec1005f06
jsschreck <jsschreck@gmail.com>
2023-02-27 16:39:26 -0700

    Bringing back in the original setup.py file
    Until the cfg issue is settled, the original setup.py file has been reintroduced.

--- setup.py
+++ setup.py
@@ -1,2 +1,11 @@
 from setuptools import setup
-setup()
+
+setup(
+  name="echo-opt",
+  version="0.1",
+  description="Earth Computer Hyperparameter Optimization",
+  author="John Schreck, David John Gagne, Charlie Becker, Gabrielle Gantos",
+  license="MIT",
+  url="https://github.com/NCAR/echo-opt",
+  packages=["echo", "echo/src"]
+)
