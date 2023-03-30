7ded6a171833481fa26981c51d771cb47247f987
David John Gagne <davidjohngagne@gmail.com>
2023-03-30 11:50:14 -0600

    Remove tensorflow from test_echo.py

--- echo/tests/test_echo.py
+++ echo/tests/test_echo.py
@@ -1,5 +1,5 @@
 from echo.src.config import config_check
-import tensorflow as tf
+# import tensorflow as tf
 import warnings
 import yaml
 import os
@@ -7,12 +7,12 @@ import os
 warnings.filterwarnings("ignore")
 
 
-def test_load_example_data():
-    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
-    assert x_train.shape == (50000, 32, 32, 3)
-    assert y_train.shape == (50000, 1)
-    assert x_test.shape == (10000, 32, 32, 3)
-    assert y_test.shape == (10000, 1)
+#def test_load_example_data():
+#    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+#    assert x_train.shape == (50000, 32, 32, 3)
+#    assert y_train.shape == (50000, 1)
+#    assert x_test.shape == (10000, 32, 32, 3)
+#    assert y_test.shape == (10000, 1)
 
 
 def test_read_keras_config():
